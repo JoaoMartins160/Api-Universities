3ead8a28c9b231c1f62ae3c527c795a646008952
Typoonja1 <101910191+Caio-Tacahashi@users.noreply.github.com>
2022-12-21 10:52:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use('/auth/login', require('./src/routes/auth'));
 
 app.use('/logout', require('./src/routes/logout'));
 
-app.use('/universities',checkToken, require('./src/routes/api/universities'));
+app.use('/universities', require('./src/routes/api/universities'));
 
 const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASS
